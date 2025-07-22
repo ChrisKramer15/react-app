70540656b2f6744ced533d73d8a80c504c1719b3
Christopher Kramer <christopher.kramer@selectquote.com>
2025-07-22 18:39:21 -0500

    completed button creation within react

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+  children: string;
+}
+
+const Button = ({ children }: Props) => {
+  return <div className="btn btn-primary">{children}</div>;
+};
+
+export default Button;
