2e80f6c97068082351b3632414b8efdd2808cad0
Christopher Kramer <christopher.kramer@selectquote.com>
2025-07-22 18:17:23 -0500

    practice with passing children into props

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["Kansas City", "Detroit", "Minnesota", "Chicago", "Cleveland"];
-
-  const handleSelectedItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectedItem}
-      />
+      <Alert>
+        Hello, <span>World!</span>
+      </Alert>
     </div>
   );
 }
