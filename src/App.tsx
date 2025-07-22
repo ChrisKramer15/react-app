0efcb2335f50a0e2a52fbe973b3ec7d7fa6b56c6
Christopher Kramer <christopher.kramer@selectquote.com>
2025-07-22 17:24:38 -0500

    made listgroup more dynamic with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["Kansas City", "Detroit", "Minnesota", "Chicago", "Cleveland"];
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
