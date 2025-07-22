b9af5fb2b1e1275c1885328f7a9889ee328fe575
Christopher Kramer <christopher.kramer@selectquote.com>
2025-07-22 17:02:24 -0500

    added event handler to listgroup

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,20 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["Kansas City", "Detroit", "Minnesota", "Chicago", "Cleveland"];
-  items = [];
+
+  //event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
