6ca70c7067f3f2249c522119ec6be6c0474651e4
Christopher Kramer <christopher.kramer@selectquote.com>
2025-07-22 17:32:19 -0500

    adding functions as props

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import ListGroup from "./components/ListGroup";
 function App() {
   let items = ["Kansas City", "Detroit", "Minnesota", "Chicago", "Cleveland"];
 
+  const handleSelectedItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectedItem}
+      />
     </div>
   );
 }
